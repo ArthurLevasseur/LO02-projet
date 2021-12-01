52e4938f2d508181195b8dda257fbc1aaf020d99
Arthur Levasseur <arthur.levasseur@utt.fr>
2021-12-01 18:22:31 +0100

    Ajout du dernier effet spécial d'une carte

--- src/Hunt.java
+++ src/Hunt.java
@@ -152,8 +152,37 @@ public class Hunt{
 		}
 		
 		else if (numCarte == 7) {
-			Joueur choix = instanceJeu.selectionnerAdversaire(instanceJeu.getEnTour(),"Choisissez un joueur. Il devra révéler son identité OU défausser une de ses cartes rumeurs");
-			return choix.accusedBucher();
+			//Joueur choix = instanceJeu.selectionnerAdversaire(instanceJeu.getEnTour(),"Choisissez un joueur. Il devra révéler son identité OU défausser une de ses cartes rumeurs");
+			
+			boolean visable;
+			
+			Scanner saisieUtilisateur = new Scanner(System.in);
+			System.out.println("De quel joueur souhaitez vous réveler l'identité ?");
+			Joueur selection = null;
+			for (int i=1 ; i<instanceJeu.getNombreJoueurs()+1 ; i++) {
+				visable = true;
+				for(CarteRumeur carte : instanceJeu.getJoueur(i-1).carteRevelees) {
+					if (carte.getNumCarte() == 6) {
+						visable = false;
+					}
+				}
+				if (instanceJeu.getJoueur(i-1).identiteAssociee.getDevoilee() == false && instanceJeu.getJoueur(i-1)!=instanceJeu.getEnTour() && visable == true) {
+					//i += 1;
+					System.out.println("Joueur " + (i) + ") " + instanceJeu.getJoueur(i-1).pseudo + " (points : " + instanceJeu.getJoueur(i-1).getPoints() + ")");
+				}
+			}
+			
+			int choix = -1;
+			
+			while (choix<0 || choix>instanceJeu.getNombreJoueurs()) {
+				choix = saisieUtilisateur.nextInt();
+				if (0<choix && choix<instanceJeu.getNombreJoueurs()+1) {
+					selection = instanceJeu.getJoueur(choix-1);
+				}
+			}
+			
+			
+			return selection.accusedBucher();
 		}
 		else if (numCarte == 8) {
 			instanceJeu.getEnTour().identiteAssociee.ReveleIdentite();
